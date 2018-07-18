c1a867ad19d23aaf6af3550bc97feb93ebd2a774
Micah Shute <micah.shute@gmail.com>
2018-07-18 19:51:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let min_index = 0;
+  for(let i = 1; i < array.length; i++){
+    if(array[i] < min){
+      min = array[i]
+      min_index = i
+    }
+  }
+  array.splice(min_index, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let sorted_arr = []
+  for(let i = 0; i < array.length; i++){
+    let minimum = findMinAndRemove(array)
+    sorted_arr.push(minimum)
+  }
+  return sorted_arr
 }
